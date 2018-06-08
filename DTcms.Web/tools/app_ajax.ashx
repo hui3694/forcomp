2b9beafefb4fc8ff7b183d9c3b56dce5c5c4f0f8
hui3694 <1054541133@qq.com>
2018-06-08 18:59:10 +0800

    6-8 news列表接口开发完成

---
+++ DTcms.Web/tools/app_ajax.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="app_ajax.ashx.cs" Class="DTcms.Web.tools.app_ajax" %>
---
+++ DTcms.Web/tools/app_ajax.ashx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+using DTcms.Web.UI;
+using DTcms.Common;
+using System.Data;
+
+namespace DTcms.Web.tools
+{
+    /// <summary>
+    /// app_ajax 的摘要说明
+    /// </summary>
+    public class app_ajax : IHttpHandler
+    {
+
+        Model.siteconfig siteConfig = new BLL.siteconfig().loadConfig();
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string action = DTRequest.GetQueryString("action");
+
+            switch (action)
+            {
+                case "get_news_list": //提交评论
+                    get_news_list(context);
+                    break;
+            }
+
+        }
+
+        private void get_news_list(HttpContext context)
+        {
+            int page = DTRequest.GetFormInt("page");
+            int count = 0;
+            DataSet ds = new BLL.news().GetList(8, page, "", "time desc", out count);
+            string strJson = DTcms.Common.JsonHelper.DataTableToJSON(ds.Tables[0]);
+
+            context.Response.Write(strJson);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
