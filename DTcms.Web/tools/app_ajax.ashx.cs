bf8b4153be4325d31d3933e8f5049173d99bbe73
hui3694 <1054541133@qq.com>
2018-06-14 17:01:24 +0800

    6-14

--- DTcms.Web/tools/app_ajax.ashx.cs
+++ DTcms.Web/tools/app_ajax.ashx.cs
@@ -26,6 +26,9 @@ namespace DTcms.Web.tools
                 case "get_news_list": //提交评论
                     get_news_list(context);
                     break;
+                case "get_category_list"://获取产品栏目列表
+                    get_category_list(context);
+                    break;
             }
 
         }
@@ -49,6 +52,39 @@ namespace DTcms.Web.tools
             string strJson = DTcms.Common.JsonHelper.DataTableToJSON(ds.Tables[0]);
 
             context.Response.Write(strJson);
+        }
+
+        private void get_category_list(HttpContext context)
+        {
+            string strJson = "";
+            strJson += "[";
+            DataSet ds = new BLL.pro_category().GetList(0, "parent_id=0", "");
+            foreach(DataRow dr in ds.Tables[0].Rows)
+            {
+                strJson += "{";
+                strJson += "\"id\":\"" + dr["id"].ToString() + "\",";
+                strJson += "\"title\":\"" + dr["title"].ToString() + "\",";
+                strJson += "\"img\":\"" + dr["img"].ToString() + "\",";
+                strJson += "\"img2\":\"" + dr["img2"].ToString() + "\",";
+
+                strJson += "\"son\":";
+                strJson += "[";
+                DataSet ds2 = new BLL.pro_category().GetList(0, "parent_id=" + dr["id"], "");
+                foreach(DataRow dr2 in ds2.Tables[0].Rows)
+                {
+                    strJson += "{";
+                    strJson += "\"id\":\"" + dr2["id"].ToString() + "\",";
+                    strJson += "\"title\":\"" + dr2["title"].ToString() + "\",";
+                    strJson += "\"img\":\"" + dr2["img"].ToString() + "\",";
+                    strJson += "\"img2\":\"" + dr2["img2"].ToString() + "\"";
+                    strJson += "},";
+                }
+                strJson = strJson.TrimEnd(',') + "]";
+                strJson +=  "},";
+            }
+            strJson = strJson.TrimEnd(',') + "]";
+            context.Response.Write(strJson.TrimEnd(','));
+
         }
 
         public bool IsReusable
